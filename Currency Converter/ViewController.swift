fd79d4e2dcad8c35b90d147823622563c36dca4c
Yaroslav Kupyak <kupjak@gmail.com>
2024-12-11 00:16:47 +0200

    Create empty project

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Yaroslav Kupyak on 10.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
